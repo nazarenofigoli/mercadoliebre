4c70cbdf09ee440e9512feb7a1dc1e595010525f
nazarenofigoli <nazarenofigoli69@gmail.com>
2023-10-19 14:40:26 -0300

    trabajo de html mercadoliebre finalizado

---
+++ practicaintegradora/app.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const app = express();
+const path = require("path");
+
+
+app.listen(3030,()=>{
+    console.log("Puerto 3030 iniciado")
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.use(express.static("public"))
